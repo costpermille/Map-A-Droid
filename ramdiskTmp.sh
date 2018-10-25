95a1a281807296bba3954e08ec1646d11ba25a81
costpermille <costpermille@users.noreply.github.com>
2018-10-25 14:45:47 -0700

    Ask user before rm -rf'ing
    This also changes from `../name` to `./name`, partially for a bit more safety. Feel free to cherrypick/revert though.

--- ramdiskTmp.sh
+++ ramdiskTmp.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
-#TODO: ask user if he REALLY wants to run the following commands
-
-rm -rf ../temp_mad
-mkdir ../temp_mad
-sudo mount -t tmpfs -o size=200M none ../temp_mad
+read -p "Are you sure you want to delete './temp_mad' and turn it into a ramdisk? (Requires sudo access) (Y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Creating ramdisk './temp_mad'"
+    rm -rf ./temp_mad
+    mkdir ./temp_mad
+    sudo mount -t tmpfs -o size=200M none ./temp_mad
+fi
+exit 0
